4238964cc21e3f1afedc7a1e4050ad9c26589bf3
mniyazz <muslimniyazbek@gmail.com>
2025-07-18 04:34:41 -0700

    hasan meeting updates

--- components/page_sections/HeroSection.tsx
+++ components/page_sections/HeroSection.tsx
@@ -11,14 +11,14 @@ export function HeroSection() {
             />
             <div className="absolute top-0 left-0 w-full h-full bg-black/60"></div>
             <div className="relative z-10 px-6 animate-fade-in-up">
-            <p className="text-base font-semibold tracking-wider" style={{ color: '#20BAD1' }}>
+            {/* <p className="text-base font-semibold tracking-wider" style={{ color: '#20BAD1' }}>
             STRATEGY. EXECUTION. RESULTS.
-            </p>
+            </p> */}
                 <h1 className="mt-4 text-4xl md:text-5xl lg:text-6xl font-extrabold tracking-tighter text-white">
                     FIND THAT MISSING PIECE
                 </h1>
-                <p className="mt-4 text-lg text-gray-200 max-w-2xl mx-auto">
-                    We Can Find What's Missing In Your Business And Address Your Needs
+                <p className="mt-4 font-semibold text-white max-w-2xl mx-auto">
+                    We Can Find What's Missing In Your Business and Address Your Needs
                 </p>
             </div>
         </section>
